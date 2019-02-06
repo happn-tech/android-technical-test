1e29b90f9bd6af3f4b68c15cd293e6e21626cc81
benjamin.bihr <benjamin.bihr@happn.fr>
2019-02-06 13:48:04 +0100

    Add description to article model

--- models/article.model.js
+++ models/article.model.js
@@ -5,6 +5,7 @@ let ArticleSchema = new Schema({
     title: { type: String, required: true },
     pubDate: { type: Date, required: true },
     link: { type: String, required: true },
+    description: { type: String, required: true },
     guid: { type: String, required: true, index: { unique: true } },
     author: { type: String, required: true },
     content: { type: String, required: false },
